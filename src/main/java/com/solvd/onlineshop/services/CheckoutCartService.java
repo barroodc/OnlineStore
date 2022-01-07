2eefa78769d43028cdd11c339d970bdf9211b63b
barroodc <53627915+barroodc@users.noreply.github.com>
2022-01-06 19:56:36 -0500

    Most recent update

--- src/main/java/com/solvd/onlineshop/services/CheckoutCartService.java
+++ src/main/java/com/solvd/onlineshop/services/CheckoutCartService.java
@@ -13,52 +13,45 @@ import java.util.concurrent.atomic.AtomicReference;
 public class CheckoutCartService implements ICheckoutCartService<CheckoutCart> {
 
     private static final Logger logger = LogManager.getLogger(CheckoutCartService.class);
+    private static PreparedStatement preparedStatement;
+
+
 
     @Override
     public CheckoutCart getCheckoutCartById(long id) {
         CheckoutCart checkoutCart = null;
         AtomicReference<String> select = new AtomicReference<>("select * from checkout_cart where id = ?");
-        try {
-            Connection connection = ConnectionPool.getConnection();
-            try {
-                PreparedStatement ps = connection.prepareStatement(select.get());
-                ps.setString(1, String.valueOf(id));
-                try (ResultSet rs = ps.executeQuery()) {
-                    if (rs.next()) {
-                        checkoutCart = new CheckoutCart(rs.getString("id"), rs.getString("mobile"), rs.getString("email"));
-                    }
-                } catch (Exception e) {
-                    logger.error(e);
-                }
-            } finally {
-                connection.close();
-            }
+        try (Connection connection = ConnectionPool.getConnection()) {
+            preparedStatement = connection.prepareStatement(select.get());
+            preparedStatement.setString(1, String.valueOf(id));
         } catch (Exception e) {
             logger.error(e);
         }
 
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            if (resultSet.next()) {
+                checkoutCart = new CheckoutCart(resultSet.getString("id"), resultSet.getString("mobile"), resultSet.getString("email"));
+            }
+        } catch (Exception e) {
+            logger.error(e);
+        }
         return checkoutCart;
 
     }
 
-    public static Vector<CheckoutCart> loadAllSuppliers(){
+    public static Vector<CheckoutCart> retrieveAllCheckoutCartServices(){
         Vector<CheckoutCart> checkoutCartList = new Vector<>();
-        String select = "select * from checkout_cart";
-        try {
-            Connection c = ConnectionPool.getConnection();
-            try {
-                Statement st = c.createStatement();
-                ResultSet rs = st.executeQuery(select);
+        AtomicReference<String> select = new AtomicReference<>("select * from checkout_cart");
+        try (Connection c = ConnectionPool.getConnection()) {
+            Statement st = c.createStatement();
+            ResultSet rs = st.executeQuery(select.get());
 
-                while (rs.next()) {
-                    String id = rs.getString("id");
-                    String mobile = rs.getString("mobile");
-                    String email = rs.getString("email");
-                    CheckoutCart checkoutCart = new CheckoutCart(id, mobile, email);
-                    checkoutCartList.add(checkoutCart);
-                }
-            } finally {
-                c.close();
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String mobile = rs.getString("mobile");
+                String email = rs.getString("email");
+                CheckoutCart checkoutCart = new CheckoutCart(id, mobile, email);
+                checkoutCartList.add(checkoutCart);
             }
         } catch (Exception e) {
             logger.error(e);
