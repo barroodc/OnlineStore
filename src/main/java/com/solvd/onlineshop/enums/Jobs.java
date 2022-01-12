46d1f472c59bbacfb0363004503c717423043935
barroodc <53627915+barroodc@users.noreply.github.com>
2022-01-11 22:57:25 -0500

    Most recent update

--- src/main/java/com/solvd/onlineshop/enums/Jobs.java
+++ src/main/java/com/solvd/onlineshop/enums/Jobs.java
@@ -1,4 +1,16 @@
 package com.solvd.onlineshop.enums;
 
 public enum Jobs {
+    CUSTOMER_SERVICE_REPRESENTATIVE("Customer Service Representative"), MARKETING_SPECIALIST("Marketing Specialist"),
+    SEO_CONTENT_WRITER("SEO Content Writer"), WAREHOUSE_WORKER("Warehouse Worker"),ADMINISTRATIVE_ASSISTANT("Administrative Assistant"),
+    RETENTION_SPECIALIST("Retention Specialist"), USER_EXPERIENCE_DESIGNER("User Experience Designer"), COMMUNITY_BUILDER("Community Builder"),
+    FRONT_END_WEB_DEVELOPER("Front End Web Developer"), SOFTWARE_DEVELOPER("Software Developer"), IT_TECHNICIAN("IT Technician"),
+    BUSINESS_ANALYST("Business Analyst"), GRAPHIC_DESIGNER("Graphic Designer"), SUPPLY_CHAIN_MANAGER("Supply Chain Manager"),
+    DIGITAL_OPERATIONS_MANAGER("Digital Operations Manager"), DIGITAL_MARKETING_MANAGER("Digital Marketing Manager"),
+    CUSTOMER_SATISFACTION_MANAGER("Customer Satisfaction Manager"), FINANCIAL_MANAGER("Financial Manager"),
+    DIRECTOR_OF_ECOMMERCE("Director of Ecommerce");
+
+    Jobs(String name) {
+
+    }
 }
--- src/main/java/com/solvd/onlineshop/enums/OrderStatus.java
+++ src/main/java/com/solvd/onlineshop/enums/OrderStatus.java
@@ -1,4 +1,11 @@
 package com.solvd.onlineshop.enums;
 
 public enum OrderStatus {
+    PENDING("Pending"), AWAITING_PAYMENT("Awaiting Payment"), AWAITING_FULFILLMENT("Awaiting Fulfillment"),
+    AWAITING_SHIPMENT("Awaiting Shipment"), AWAITING_PICKUP("Awaiting Pickup"), PARTIALLY_SHIPPED("Partially Shipped"),
+    COMPLETED("Completed"), SHIPPED("Shipped"), CANCELLED("Cancelled"), DECLINED("Declined"), REFUNDED("Refunded"),
+    DISPUTED("Disputed"), MANUAL_VERIFICATION_REQUIRED("Manual Verification Required"), PARTIALLY_REFUNDED("Partially Refunded");
+
+    OrderStatus(String name) {
+    }
 }
