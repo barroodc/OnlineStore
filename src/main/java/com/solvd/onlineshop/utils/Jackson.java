f1c73ae71f26cac2f3e56c66515921610bcb1928
barroodc <53627915+barroodc@users.noreply.github.com>
2022-01-23 15:32:30 -0500

    most recent update

--- src/main/java/com/solvd/onlineshop/utils/Jackson.java
+++ src/main/java/com/solvd/onlineshop/utils/Jackson.java
@@ -1,7 +1,5 @@
 package com.solvd.onlineshop.utils;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
@@ -13,25 +11,19 @@ import java.io.File;
 public class Jackson {
 
     private static final Logger LOGGER = LogManager.getLogger(Jackson.class);
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
 
-
-    /*public static String serialize(File file){
-        String json = "";
-        ObjectMapper objectMapper = new ObjectMapper();
+    private static void serialize(File file) {
         try {
-            file = new File(file.toString());
-            json = objectMapper.writeValueAsString(file);
-        } catch (JsonProcessingException e) {
+            String json = MAPPER.writeValueAsString(file);
+            LOGGER.info(json);
+        } catch (JsonProcessingException e){
             LOGGER.error(e);
         }
-
-        return json;
     }
 
-     */
-
-    /*public static <T> T deserialize(String jsonString, Class<T> cls){
+    public static <T> T deserialize(String jsonString, Class<T> cls){
         T result = null;
         ObjectMapper objectMapper = new ObjectMapper();
         try {
@@ -41,11 +33,4 @@ public class Jackson {
         }
         return result;
     }
-
-     */
-
-    public static void main(String[] args) {
-
-    }
-
 }
