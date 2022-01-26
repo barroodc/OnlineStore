1919e0a094b32777c9dfddfbb2470cec34f62075
barroodc <53627915+barroodc@users.noreply.github.com>
2022-01-26 11:48:51 -0500

    Using test classes/files from carina repo

---
+++ src/test/java/com/solvd/onlineshop/APISampleTest.java
@@ -0,0 +1,62 @@
+package com.solvd.onlineshop;
+import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.solvd.onlineshop.api.DeleteUserMethod;
+import com.solvd.onlineshop.api.GetUserMethods;
+import com.solvd.onlineshop.api.PostUserMethod;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.testng.annotations.Test;
+
+import com.qaprosoft.apitools.validation.JsonCompareKeywords;
+import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
+import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
+import com.qaprosoft.carina.core.foundation.utils.tag.Priority;
+import com.qaprosoft.carina.core.foundation.utils.tag.TestPriority;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class APISampleTest implements IAbstractTest {
+
+    private static final Logger LOGGER = LogManager.getLogger(APISampleTest.class);
+
+    public void testCreateUser() throws Exception {
+        LOGGER.info("test");
+        setCases("4555,54545");
+        PostUserMethod api = new PostUserMethod();
+        api.expectResponseStatus(HttpResponseStatusType.CREATED_201);
+        api.callAPI();
+        api.validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    public void testCreateUserMissingSomeFields() throws Exception {
+        PostUserMethod api = new PostUserMethod();
+        api.getProperties().remove("name");
+        api.getProperties().remove("username");
+        api.expectResponseStatus(HttpResponseStatusType.CREATED_201);
+        api.callAPI();
+        api.validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    public void testGetUsers() {
+        GetUserMethods getUsersMethods = new GetUserMethods();
+        getUsersMethods.expectResponseStatus(HttpResponseStatusType.OK_200);
+        getUsersMethods.callAPI();
+        getUsersMethods.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey());
+        getUsersMethods.validateResponseAgainstSchema("api/users/_get/rs.schema");
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P1)
+    public void testDeleteUsers() {
+        DeleteUserMethod deleteUserMethod = new DeleteUserMethod();
+        deleteUserMethod.expectResponseStatus(HttpResponseStatusType.OK_200);
+        deleteUserMethod.callAPI();
+        deleteUserMethod.validateResponse();
+    }
+
+}
